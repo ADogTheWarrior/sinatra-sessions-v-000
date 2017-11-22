04a959689ca7c27c9d6a0e3bd99814115f897800
Adam Fudge <coolfudge@gmail.com>
2017-11-22 22:18:41 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -30,15 +30,18 @@ class App < Sinatra::Base
   end
 
   get '/fetch' do
-    "You did it! session[:foo] value: #{session[:foo]}.\nMove on to Part II of this lab at '/second_exercise' "
+    "You did it! session[:foo] value: #{session[:foo]}.
+    \nMove on to Part II of this lab at '/second_exercise' "
   end
 
   get '/second_exercise' do
-    "Your second lesson will be to set your session :id key to a value.\nIn the route: /set_session, write a line of code that sets the session[:id] equal to 1.\nThen, navigate to the '/set_session' path"
+    "Your second lesson will be to set your session :id key to a value.
+    \nIn the route: /set_session, write a line of code that sets the session[:id] equal to 1.
+    \nThen, navigate to the '/set_session' path"
   end
 
   get '/set_session' do
-    #set session id here
+    session[:id] = 1
 
     if session[:id] == 1
       # "Session ID set. It's currently set to #{session[:id]}."
